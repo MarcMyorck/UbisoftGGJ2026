d7805d533078f276e0ce4c1a8745d6f4c54f8dc8
MarcMyorck <riewe.marc@web.de>
2026-01-31 16:34:58 +0100

    Added new animation for enemy attack

--- Assets/Scripts/EnemySpriteTurn.cs
+++ Assets/Scripts/EnemySpriteTurn.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class EnemySpriteTurn : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Animator anim = GetComponent<Animator>(); 
-        anim.Play("PlayerIdle");
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.rotation = Quaternion.Inverse(transform.parent.rotation) * transform.rotation;
     }
 }
